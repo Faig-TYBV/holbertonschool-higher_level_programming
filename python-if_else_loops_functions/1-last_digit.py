d5d50118534630dff3d896a20baa1faafe8b71c2
Faig-TYBV <tayibovfaig@gmail.com>
2025-11-28 15:48:18 +0000

    phw

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+ld=number%10
+print(f"Last digit of {number} is {ld} ", end='')
+if ld==0:
+    print("and is 0")
+elif ld>5:
+    print("and is greater than 5")
+else:
+    print("and is less than 6 and not 0")
