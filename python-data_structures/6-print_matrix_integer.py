4b3abb14e5681fa65e066f00731612f5d4583e3c
Faig-TYBV <tayibovfaig@gmail.com>
2025-12-02 14:41:01 +0000

    pdt

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -5,6 +5,7 @@ def print_matrix_integer(matrix=[[]]):
         ln = len(lst) - 1
         for i in range(ln):
             print("{:d}".format(lst[i]), end=' ')
-        print("{:d}".format(lst[ln]))
+        if ln >= 0:
+            print("{:d}".format(lst[ln]))
     if len(matrix) == 0:
         print(" ")
