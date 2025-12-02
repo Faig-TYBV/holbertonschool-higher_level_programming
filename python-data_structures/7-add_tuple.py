b8fc2fe0d7e7324c5bc75f4cdab609bdd5be355f
Faig-TYBV <tayibovfaig@gmail.com>
2025-12-02 15:46:33 +0000

    pdt

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
 # learning python
 def add_tuple(tuple_a=(), tuple_b=()):
-    new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    sum1 = 0 
+    sum2 = 0
+    if len(tuple_a) > 1:
+        sum1 += tuple_a[0]
+        sum2 += tuple_a[1]
+    elif len(tuple_a) > 0:
+        sum1 += tuple_a[0]
+    if len(tuple_b) > 1:
+        sum1 += tuple_b[0]
+        sum2 += tuple_b[1]
+    elif len(tuple_b) > 0:
+        sum1 += tuple_b[0]
+    new_tuple = (sum1, sum2)
     return new_tuple
-
