dab332cbc11c62d0538bb3c958563aef9a6aaf96
Faig-TYBV <tayibovfaig@gmail.com>
2025-12-02 14:36:54 +0000

    pdt

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,7 @@
 # learnig python
 def print_matrix_integer(matrix=[[]]):
     for lst in matrix:
-        for i in lst:
-            print("{:d}".format(i), end=' ')
-        print("")
+        ln = len(lst) - 1
+        for i in range(ln):
+            print("{:d}".format(lst[i]), end=' ')
+        print("{:d}".format(lst[ln]))
