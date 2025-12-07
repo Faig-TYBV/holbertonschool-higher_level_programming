18f4cfe4399e4df40c3f9de8d484076c90cc2b38
ftayibov20472 <ftayibov20472@ada.edu.az>
2025-12-07 19:08:44 +0400

    pmtd

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 # learning python
+from functools import reduce
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-
     return reduce(
         lambda a, b: a if a_dictionary[a] >= a_dictionary[b] else b,
         a_dictionary.keys()
