d833a93d3e0c05101a1bd14a8d2c45cca9a6129f
ftayibov20472 <ftayibov20472@ada.edu.az>
2025-12-07 19:51:43 +0400

    pmtd

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    """Prints the first x elements of a list that are integers.
+
+    Args:
+        my_list (list): The list to print elements from.
+        x (int): The number of elements to consider from the list.
+
+    Returns:
+        int: The number of integer elements actually printed.
+    """
+    count = 0
+    print()
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (ValueError, TypeError, IndexError):
+            continue
+    return count
