6236583e7d2215ecef7dc323ef9f311182a911e5
ftayibov20472 <ftayibov20472@ada.edu.az>
2025-12-07 19:57:28 +0400

    pmtd

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    """Divides elements of two lists element by element.
+
+    Args:
+        my_list_1 (list): The first list of numbers.
+        my_list_2 (list): The second list of numbers.
+        list_length (int): The number of elements to consider from both lists.
+
+    Returns:
+        list: A new list containing the results of the divisions.
+    """
+    result = []
+    for i in range(list_length):
+        try:
+            division = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            division = 0
+            print("division by 0")
+        except (TypeError, ValueError):
+            division = 0
+            print("wrong type")
+        except IndexError:
+            division = 0
+            print("out of range")
+        finally:
+            result.append(division)
+    return result
