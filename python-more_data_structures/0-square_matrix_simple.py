73e4d7e0b2c33982f25f50b12cedb574471e6b89
ftayibov20472 <ftayibov20472@ada.edu.az>
2025-12-07 18:31:34 +0400

    pmtd

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # learning python
-def square_matrix_simple(matrix = []):
-  ans = list()
-  for i in matrix:
-    ans.append(list(map(lambda x: x**2, i)))
-  return ans
+def square_matrix_simple(matrix=[]):
+    ans = []
+    for row in matrix:
+        ans.append(list(map(lambda x: x ** 2, row)))
+    return ans
