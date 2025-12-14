d19531e5fd4c8141cf84c3a7a65d90494d90f8f0
Faig-TYBV <tayibovfaig@gmail.com>
2025-12-14 17:46:51 +0000

    pi

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-""" This module contains a function """
+""" a function """
 
 
-def is_kind_of_class(obj, a_class):
-    """ Return smth """
-    return isinstance(obj, a_class)
+def inherits_from(obj, a_class):
+    """ issubclass """
+    return issubclass(type(obj), a_class) and not type(obj) is a_class
