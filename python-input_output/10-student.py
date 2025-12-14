13b945cf17e950ef375ecb6de31fedfbe540d3bb
Faig-TYBV <tayibovfaig@gmail.com>
2025-12-14 18:29:00 +0000

    pi

---
+++ python-input_output/10-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" This module """
+
+
+class Student:
+    """ Student class """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Get a dictionary representation of the Student."""
+        if (attrs.__class__ is list and
+                all(ele.__class__ is str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
