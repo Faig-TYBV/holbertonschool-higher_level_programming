a8163593e0f96c422f7cc5b9ad2f0cba53ed84b0
Faig-TYBV <tayibovfaig@gmail.com>
2025-12-14 08:30:18 +0000

    pmc

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"Rectangle module"
+
+
+class Rectangle:
+    "Rectangle class"
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        "Width"
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        "Set width"
+
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        "Heigth"
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        "Set width"
+
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        "Area"
+
+        return self.__width * self.__height
+
+    def perimeter(self):
+        "Perimeter"
+
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return self.__widht * 2 + self.__height * 2
