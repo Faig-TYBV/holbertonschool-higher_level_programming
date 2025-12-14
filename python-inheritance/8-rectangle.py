a4a275ee2d29202d77ac7f7a55273036ba7ccea0
Faig-TYBV <tayibovfaig@gmail.com>
2025-12-14 18:07:19 +0000

    pi

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-""" a class """
+"""7777"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-class BaseGeometry:
-    """ geoclass """
-    def area(self):
-        """ it is not implemented yet """
-        raise Exception("area() is not implemented")
 
-    def integer_validator(self, name, value):
-        """ validate integers here """
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+class Rectangle(BaseGeometry):
+    """i am the best"""
+    
+    def __init__(self, width, height):
+        """123456"""
+
+        self.integer_validator("width", width)
+        self.__width = width
+
+        self.integer_validator("width", width)
+        self.__height = height
