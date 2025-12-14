2887b9cd952a789c58ed0f5afa5b3d28533d77b2
Faig-TYBV <tayibovfaig@gmail.com>
2025-12-14 18:35:47 +0000

    pi

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
 """7777"""
-
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
-
 
 
 class Rectangle(BaseGeometry):
     """i am the best"""
-    
+
     def __init__(self, width, height):
         """123456"""
 
         self.integer_validator("width", width)
         self.__width = width
-
         self.integer_validator("width", width)
         self.__height = height
