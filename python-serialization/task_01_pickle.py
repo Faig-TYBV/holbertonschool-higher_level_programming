8df13bf87ddfed0fa2cc3f7cd375648ba30e0be9
Faig-TYBV <tayibovfaig@gmail.com>
2025-12-20 23:43:45 +0000

    serialization

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+# Pickle
+
+
+import pickle
+
+
+class CustomObject:
+    '''Class'''
+
+    def __init__(self, n, a, iS):
+        # Init
+
+        self.name = n
+        self.age = a
+        self.is_student = iS
+
+    def display(self):
+        # Information
+
+        print("Name: {}\nAge: {}\nIs Student: {}"
+              .format(self.name, self.age, self.is_student))
+
+    def serialize(self, filename):
+        # Serializing instances
+
+        with open(filename, "wb") as file:
+            pickle.dump(self, file)
+
+    @classmethod
+    def deserialize(cls, filename):
+        # Deserialization
+
+        with open(filename, "rb") as file:
+            return pickle.load(file)
