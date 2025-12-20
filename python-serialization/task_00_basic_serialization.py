c2a43f3e2a020dc0ad8d7e616b32d0c69e7ba25b
Faig-TYBV <tayibovfaig@gmail.com>
2025-12-20 20:19:00 +0000

    learning serialization

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -8,10 +8,11 @@ def serialize_and_save_to_file(data, filename):
     # Serialization
 
     with open("data.pkl", "wb") as filename:
-    pickle.dump(data, filename)
+        pickle.dump(data, filename)
 
 def load_and_deserialize(filename):
     # Deserialization
-
-    loaded_data = pickle.load(filename)
+    
+    with open("data.pkl", "rb") as file
+        loaded_data = pickle.load(filename)
     return loaded_data
